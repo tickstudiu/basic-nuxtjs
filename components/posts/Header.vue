0d8d33396f6c9ef58a1c8ae66bb491f72bdecb0e
Wanchalerm S <tickstudiu@gmail.com>
2020-08-21 00:15:11 +0700

    learn how to using loop and create data

--- components/posts/Header.vue
+++ components/posts/Header.vue
@@ -1,32 +1,34 @@
 <template>
   <div>
     <b-carousel controls indicators id="silder">
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide
-        caption="Second slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=58"
-      ></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            caption="Thrid slide"
-            text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-            src="https://picsum.photos/1024/480/?image=55"
-          />
-        </template>
-      </b-carousel-slide>
+      <div v-for="item in items" :key="item.message">
+        <b-carousel-slide
+          :caption=item.caption
+          :text=item.text
+          :img-src=item.image
+        ></b-carousel-slide>
+      </div>
     </b-carousel>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          captions: 'First slide',
+          image: 'https://picsum.photos/1024/480/?image=52',
+          text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
+        },
+        {
+          captions: 'Second slide',
+          image: 'https://picsum.photos/1024/480/?image=58',
+          text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
+        },
+      ],
+    }
+  },
+}
+</script>
